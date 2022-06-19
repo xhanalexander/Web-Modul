617ca3685432b6d65f111dab553ebef2ac8618a6
xhanalexander <57014322+xhanalexander@users.noreply.github.com>
2022-06-19 17:11:13 +0700

    Update crudeDB.sql

--- pertemuan11/crudeDB.sql
+++ pertemuan11/crudeDB.sql
@@ -3,11 +3,8 @@ CREATE DATABASE crudeDB;
 USE crudeDB;
 
 CREATE TABLE user (
-   "id"             INT(11) NOT NULL AUTO_INCREMENT,
-   "names"          VARCHAR(50) NOT NULL,
-   "email"          VARCHAR(50) NOT NULL,
-   "mobile"         VARCHAR(15) NOT NULL,
-   PRIMARY KEY ("id")
+   id             INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   names          VARCHAR(50) NOT NULL,
+   email          VARCHAR(50) NOT NULL,
+   mobile         VARCHAR(15) NOT NULL
 );
-
-
